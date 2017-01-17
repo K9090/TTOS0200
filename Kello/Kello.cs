ea77a4b8605f6fa8da9286dfe9f3f53cdba9ace6
Timo Hakamäki <K9090@student.jamk.fi>
2017-01-18 01:35:26 +0200

    Teht6/Kello ok

---
+++ Kello/Kello.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    class Kello
+    {
+        public bool IsRunning { get; set; }
+        int hour, min, day = 1;
+        public int TimeH {
+            get { return hour; }
+            set
+            {
+                hour = value;
+                if (hour >= 24)
+                {
+                    day++;
+                    hour = hour - 24 * day;
+                }
+            }
+        }
+        public int TimeM {
+            get { return min; }
+            set
+            {
+                min = value;
+                if (min >= 60)
+                {
+                    hour++;
+                    min = min - 60;
+                }
+            }
+        }
+        public bool IsOnTime { get; set; }
+
+        public void SetTime()
+        {
+            Console.Write("Syötä tunnit: ");
+            TimeH = Int32.Parse(Console.ReadLine());
+            Console.Write("Syötä minuutit: ");
+            TimeM = Int32.Parse(Console.ReadLine());
+        }
+        public override string ToString()
+        {
+            string showTime = TimeH + " tuntia, " + TimeM + " minuuttia.";
+            return showTime;
+        }
+    }
+}
---
+++ Kello/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JAMK.IT;
+
+namespace Teht6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kello time = new Kello();
+            time.IsRunning = true;
+            time.IsOnTime = true;
+
+            time.SetTime();
+
+            Console.WriteLine();
+            Console.WriteLine(time.ToString());
+            Console.WriteLine();
+        }
+    }
+}
