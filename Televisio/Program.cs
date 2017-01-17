688427d785a8c590c68e565bd04444382b6e27fe
K9090 <K9090@student.jamk.fi>
2017-01-17 14:55:08 +0200

    Televisio ok

---
+++ Televisio/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JAMK.IT;
+
+namespace TelevisioTeht
+{
+    class Program
+    {
+        static void Main()
+        {
+            TestaaTelevisio();
+        }
+        static void TestaaTelevisio()
+        {
+            Televisio tv = new JAMK.IT.Televisio();
+            tv.PowerOn = true;
+            tv.Volume = 20;
+            tv.Channel = 3;
+            tv.Wifi = true;
+
+            Console.WriteLine("Television virta on päällä {0}", tv.PowerOn);
+            Console.WriteLine("Television äänenvoimakkuus {0}", tv.Volume);
+            Console.WriteLine("Television kanava {0}", tv.Channel);
+            Console.WriteLine("Television Wifi on päällä {0}", tv.Wifi);
+            //mitä tapahtuu, jos ohjelmanuppi pyörähtää ympäri
+            tv.Channel = 1001;
+            Console.WriteLine("Television ohjelma {0}", tv.Channel);
+        }
+    }
+}
---
+++ Televisio/Televisio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    class Televisio
+    {
+        public bool PowerOn { get; set; }
+        public bool Wifi { get; set; }
+        int vol;
+        public int Volume {
+            get { return vol; }
+            set
+            {
+                vol = value;
+                if (vol < 0 || vol > 100)
+                {
+                    vol = 0;
+                }
+            }
+        }
+        int chan;
+        public int Channel {
+            get { return chan; }
+            set
+            {
+                chan = value;
+                if (chan < 1)
+                {
+                    chan = 1000;
+                }
+                else if (chan > 1000)
+                {
+                    chan = 1;
+                }
+            }
+        }
+    }
+}
