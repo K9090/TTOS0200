588c211b747116bc1cd1590d80b7310001ab0a29
Timo Hakamäki <K9090@student.jamk.fi>
2017-01-18 18:27:31 +0200

    Teht4vko3 ok

---
+++ Teht4vko3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JAMK.IT;
+
+namespace PersonApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a one Vehicle object
+            Vehicle automobile = new Vehicle();
+            automobile.Name = "Honda";
+            automobile.Model = "Civic";
+            automobile.ModelYear = 2006;
+            automobile.Color = "Metallic Blue";
+            Console.WriteLine(automobile.ToString());
+
+            // create a one Bike Object
+            Bike bike1 = new Bike();
+            bike1.Name = "Tunturi";
+            bike1.Model = "Raider";
+            bike1.ModelYear = 2000;
+            bike1.Color = "Black";
+            bike1.HasGears = true;
+            bike1.GearModel = "Wanderer";
+            Console.WriteLine(bike1.ToString());
+
+            // create a one Bike Object
+            Bike bike2 = new Bike();
+            bike2.Name = "-some chinesey crap-";
+            bike2.Model = "-fear for your life-";
+            bike2.ModelYear = 2030;
+            bike2.Color = "Rusted Grey";
+            bike2.HasGears = false;
+            bike2.GearModel = "";
+            Console.WriteLine(bike2.ToString());
+
+            // create another Boat object
+            Boat boat = new Boat();
+            boat.Name = "Works Better";
+            boat.Model = "On Land";
+            boat.ModelYear = 1650;
+            boat.Color = "Brown";
+            boat.BoatType = "Motokayak";
+            boat.SeatCount = 3;
+            Console.WriteLine(boat.ToString());
+        }
+    }
+}
---
+++ Teht4vko3/Teht4vko3.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    class Vehicle
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int ModelYear { get; set; }
+        public string Color { get; set; }
+
+        // default constructor
+        public Vehicle()
+        {
+        }
+
+        // constructor takes employee name and model as parameters
+        public Vehicle(string name, string model)
+        {
+            Name = name;
+            Model = model;
+        }
+
+        // return Vehicle data as a one string, override Object class ToString()-method
+        public override string ToString()
+        {
+            return "Name: " + Name + ", Model: " + Model + ", Model year: " + ModelYear + ", Color: " + Color;
+        }
+    }
+
+    class Bike : Vehicle
+    {
+        public bool HasGears { get; set; }
+        public string GearModel { get; set; }
+
+        // default constructor
+        public Bike()
+        {
+        }
+
+        // parametric constuctor calls base class constructor with parameters
+        public Bike(string name, string model, bool hasGears, string gearModel)
+            : base(name, model)
+        {
+            HasGears = hasGears;
+            GearModel = gearModel;
+        }
+
+        // override base class ToString()-method
+        public override string ToString()
+        {
+            return base.ToString() + ", HasGears: " + HasGears + ", Gear Model: " + GearModel;
+        }
+    }
+
+    class Boat : Vehicle
+    {
+        public string BoatType { get; set; }
+        public int SeatCount { get; set; }
+
+        // default constructor
+        public Boat()
+        {
+        }
+
+        // parametric constuctor calls base class constructor with parameters
+        public Boat(string name, string model, string boatType, int seatCount)
+            : base(name, model)
+        {
+            BoatType = boatType;
+            SeatCount = seatCount;
+        }
+
+
+        // override base class ToString()-method
+        public override string ToString()
+        {
+            return base.ToString() + ", Boat type: " + BoatType + ", Seat count: " + SeatCount;
+        }
+    }
+}
