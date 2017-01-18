8416f8884869af691bae5c323284dd690a8f85e3
Timo Hakamäki <K9090@student.jamk.fi>
2017-01-18 22:58:47 +0200

    Teht5vko3 nearly ok, missing the possibility to use decimal values in setting the 'frequency'

---
+++ Teht5vko3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JAMK.IT;
+
+namespace Teht5vko3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SetVolAndFreq();
+        }
+
+        static void SetVolAndFreq()
+        {
+            Radio radio = new Radio();
+            radio.Volume = 2;
+            radio.PowerOn = true;
+
+            Console.WriteLine("Power is on: {0}", radio.PowerOn);
+
+            if (radio.PowerOn == true)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.Write("Give a new volume value (0-9): ");
+                    radio.Volume = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter a new frequency (2000.0 - 26000.0): ");
+                    radio.Frequency = Double.Parse(Console.ReadLine());
+
+                    Console.WriteLine(radio.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ Teht5vko3/Teht5vko3.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    class Radio
+    {
+        public bool PowerOn { get; set; }
+
+        private int volume = 1;
+        private int isVolValid = 1;
+        private const int maxVol = 9;
+        private const int minVol = 0;
+        private int IsVolValid
+        {
+            get { return isVolValid; }
+        }
+        public int Volume
+        {
+            get { return volume; }
+            set
+            {
+                volume = value;
+                if (value < minVol)
+                {
+                    isVolValid = 0;
+                    volume = minVol;
+                }
+                else if (value > maxVol)
+                {
+                    isVolValid = 2;
+                    volume = maxVol;
+                }
+                else
+                {
+                    isVolValid = 1;
+                    volume = value;
+                }
+            }
+        }
+
+        private int isFreqValid = 1;
+        public int IsFreqValid
+        {
+            get { return isFreqValid; }
+        }
+        private const double maxFreq = 26000.0;
+        private const double minFreq = 2000.0;
+        private double frequency = 2000.0;
+        public double Frequency
+        {
+            get { return frequency; }
+            set
+            {
+                frequency = value;
+                if (value < minFreq)
+                {
+                    isFreqValid = 0;
+                    frequency = minFreq;
+                }
+                else if (value > maxFreq)
+                {
+                    isFreqValid = 2;
+                    frequency = maxFreq;
+                }
+                else
+                {
+                    isFreqValid = 1;
+                    frequency = value;
+                }
+            }
+        }
+
+        public string VolMsg()
+        {
+
+            if (IsVolValid == 0)
+            {
+                return "Volume too low  - Radio min volume is set to: " + minVol;
+            }
+            else if (IsVolValid == 2)
+            {
+                return "Volume too potent - Radio max volume is set to: " + maxVol;
+            }
+            else
+            {
+                return "Radio volume set to: " + Volume;
+            }
+
+        }
+
+        public string FreqMsg()
+        {
+            if (IsFreqValid == 0)
+            {
+                return "Attempted frequency too low  - Lowest possible frequency is set to: " + minFreq;
+            }
+            else if (IsFreqValid == 2)
+            {
+                return "Attempted frequency too high  - Highest possible frequency is set to: " + maxFreq;
+            }
+            else
+            {
+                return "Radio frequency set to: " + Frequency;
+            }
+        }
+
+        public override string ToString()
+        {
+            return VolMsg() + "\n" + FreqMsg() + "\n";
+        }
+    }
+}
