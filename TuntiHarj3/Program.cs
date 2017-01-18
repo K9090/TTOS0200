3b3de8262d932052f7ccfe4d7bb535ab3e7fa861
K9090 <k9090@student.jamk.fi>
2017-01-18 16:07:34 +0200

    TuntiHarj3 now really ok

---
+++ TuntiHarj3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JAMK.IT;
+
+namespace PersonApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a one Employee object
+            Employee person = new Employee();
+            person.FirstName = "Kirsi";
+            person.LastName = "Kernel";
+            person.Profession = "Kylähullu";
+            person.Salary = 3000;
+            Console.WriteLine(person.ToString());
+
+            // create a one Boss Object
+            Boss teacher = new Boss();
+            teacher.FirstName = "Teppo";
+            teacher.LastName = "Konsoli";
+            teacher.Profession = "Ope";
+            teacher.Salary = 4000;
+            teacher.Car = "Lada";
+            teacher.Bonus = 6000;
+            Console.WriteLine(teacher.ToString());
+
+            // create another Employee object
+            Employee person2 = new Employee();
+            person2.FirstName = "Minna";
+            person2.LastName = "Husso";
+            person2.Profession = "Velhotar";
+            person2.Salary = 10000;
+            Console.WriteLine(person2.ToString());
+        }
+    }
+}
---
+++ TuntiHarj3/Teht3vko3.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Profession { get; set; }
+        public int Salary { get; set; }
+
+        // default constructor
+        public Employee()
+        {
+        }
+
+        // constructor takes employee firstname and lastname as a parameter
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        // return Employee data as a one string, override Object class ToString()-method
+        public override string ToString()
+        {
+            return "Name: " + FirstName + " " + LastName + ", Profession: " + Profession + ", Salary: " + Salary;
+        }
+    }
+    
+    class Boss : Employee
+    {
+        public string Car { get; set; }
+        public int Bonus { get; set; }
+
+        // default constructor
+        public Boss()
+        {
+        }
+
+        // parametric constuctor calls base class constructor with parameters
+        public Boss(string firstName, string lastName, string car, int bonus)
+            : base(firstName, lastName)
+        {
+            Car = car;
+            Bonus = bonus;
+        }
+
+        // override base class ToString()-method
+        public override string ToString()
+        {
+            return base.ToString() + " Car: " + Car + ", Bonus: " + Bonus;
+        }
+    }
+}
