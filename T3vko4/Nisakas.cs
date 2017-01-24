dffdb719a4f2ba7d2228c25533491bd5e204ad1b
Timo Hakamäki <K9090@student.jamk.fi>
2017-01-25 01:24:32 +0200

    T3vko4 ok

---
+++ T3vko4/Nisakas.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    abstract class Nisakas
+    {
+        private int age;
+        public int Age {
+            get { return age; }
+            set
+            {
+                age = value;
+            }
+        }
+
+        public Nisakas()
+        {
+
+        }
+
+        public string Move()
+        {
+            return string.Format("is moving");
+        }
+
+        public override string ToString()
+        {
+            return "Age: " + Age;
+        }
+    }
+
+    class Human : Nisakas
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+
+        public Human()
+        {
+
+        }
+
+        public int Grow()
+        {
+            int age = Age;
+            age++;
+            return age;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\nName: " + Name + "\nHeight: " + Height + "\nWeight: " + Weight;
+        }
+    }
+
+    class Adult : Human
+    {
+        public string Car { get; set; }
+
+        public Adult()
+        {
+
+        }
+
+        public new string Move()
+        {
+            return string.Format("is walking");
+        }
+
+        public override string ToString()
+        {
+            return base.ToString()  + "\nCar: " + Car + "\n" + Move();
+        }
+    }
+
+    class Infant : Human
+    {
+        public bool HasDiaper { get; set; }
+
+        public Infant()
+        {
+
+        }
+
+        public new string Move()
+        {
+            return string.Format("is crawling");
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\nDiaper: " + HasDiaper + "\n" + Move();
+        }
+    }
+}
---
+++ T3vko4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JAMK.IT;
+
+namespace T3vko4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adult adult = new Adult();
+            
+            adult.Name = "Ugga";
+            adult.Age = 23;
+            adult.Height = 160;
+            adult.Weight = 80;
+            adult.Car = "Lada";
+
+            Infant infant = new Infant();
+            
+            infant.Name = "Krugg";
+            infant.Age = 1;
+            infant.Height = 70;
+            infant.Weight = 10;
+            infant.HasDiaper = true;
+
+            Adult adult1 = new Adult();
+
+            adult1.Name = "Umbah";
+            adult1.Age = 26;
+            adult1.Height = 170;
+            adult1.Weight = 70;
+            adult1.Car = "Flintstones' Mobile";
+
+            Console.WriteLine(adult.ToString() + "\n");
+
+            Console.WriteLine(infant.ToString() + "\n");
+
+            Console.WriteLine(adult1.ToString() + "\n");
+
+            Console.WriteLine(infant.Name + " just experienced his {0} birthday!", infant.Grow());
+
+            Console.WriteLine();
+        }
+    }
+}
