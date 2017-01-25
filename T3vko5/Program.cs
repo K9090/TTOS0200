f999c9b3296c1cb6b64a75061e610a5376a5f061
K9090 <K9090@student.jamk.fi>
2017-01-25 17:23:22 +0200

    T3vko5 continues

--- T3vko5/Program.cs
+++ T3vko5/Program.cs
@@ -17,17 +17,17 @@ namespace JAMK.IT.T3vko5
             Hearts heart9 = new Hearts();
             heart9.Number = 9;
             heart9.Suit = "Hearts";
-            heart9.GiveNumber();
+            heart9.LoopNumbers(1);
+
+            Hearts heart10 = new Hearts();
+            heart10.Number = 10;
+            heart10.Suit = "Hearts";
 
             List<Card> cardDeck = new List<Card>();
 
             cardDeck.Add(new Hearts());
 
             Console.WriteLine("Loop through the card deck:");
-            foreach (Card card in cardDeck)
-            {
-                card.GiveNumber();
-            }
         }
     }
 }
--- T3vko5/T3vko5.cs
+++ T3vko5/T3vko5.cs
@@ -8,17 +8,45 @@ namespace JAMK.IT
 {
     abstract class Card
     {
-        public int Number { get; set; }
+        private List<Card> cards;
+        public List<Card> Cards { get { return cards; } }
+        private int minNumber = 1;
+        private int maxNumber = 13;
+        private int number;
+        public int Number {
+            get { return number; }
+            set
+            {
+                if (number >= minNumber || number <= maxNumber)
+                {
+                    number = value;
+                }
+                else
+                {
+                    Console.WriteLine(number + " will not do"); 
+                }
+            }
+        }
         public virtual string Suit { get; set; }
 
         public Card()
         {
 
         }
-
         public Card(int number)
         {
             Number = number;
+        }
+        public void AddCard(Card card)
+        {
+            cards.Add(card);
+        }
+        public void LoopNumbers(int number)
+        {
+            for (int i = 0; i < 13; i++)
+            {
+                number++;
+            }
         }
 
         public abstract void GiveNumber();
