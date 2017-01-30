9f6faff04c1a4302b31eebf6c5bc7302e45cca92
K9090 <K9090@student.jamk.fi>
2017-01-30 13:48:08 +0200

    T3Lab7 started

---
+++ T3Lab7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.T3Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Input();
+        }
+
+        static void Input()
+        {
+            FileIO fileIO = new FileIO();
+
+            fileIO.SaveToFile();
+            fileIO.ReadFromFile();
+        }
+    }
+}
---
+++ T3Lab7/T3Lab7.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    class FileIO
+    {
+        public string Input { get; set; }
+
+        public void SaveToFile()
+        {
+            StreamWriter outputFile = null;
+            try
+            {
+                if (Input )
+                {
+
+                }
+                outputFile = new StreamWriter(@"D:\K9090\Olio\Repo\harjoitukset\T3ints.txt");
+                outputFile = new StreamWriter(@"D:\K9090\Olio\Repo\harjoitukset\T3doubles.txt");
+                do
+                {
+                    Console.Write("Insert numbers: ");
+                    Input = Console.ReadLine();
+                    outputFile.WriteLine(Input);
+
+                } while (Input != String.Empty);
+
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Some exception happened!");
+                Console.WriteLine(ex.Message); // Access to the path 'D:\K9090\Olio\Repo\harjoitukset\test.txt' is denied.
+            }
+            finally
+            {
+                // check for null because OpenWrite might have failed
+                if (outputFile != null)
+                {
+                    outputFile.Close();
+                }
+            }
+        }
+
+        public void ReadFromFile()
+        {
+            try
+            {
+                string ints = File.ReadAllText(@"D:\K9090\Olio\Repo\harjoitukset\T3ints.txt");
+                Console.WriteLine("Contents of T3ints.txt:\n" + ints);
+                string doubles = File.ReadAllText(@"D:\K9090\Olio\Repo\harjoitukset\T3ints.txt");
+                Console.WriteLine("Contents of T3doubles.txt:\n" + doubles);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found (FileNotFoundException)");
+            }
+        }
+    }
+}
