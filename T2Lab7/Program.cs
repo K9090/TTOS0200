6f9d27332e5acea4ff9382011a758870cb0d3a93
K9090 <K9090@student.jamk.fi>
2017-01-30 13:28:42 +0200

    T2Lab7 ok, except for sorting

---
+++ T2Lab7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.T2Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Input();
+        }
+
+        static void Input()
+        {
+            FileIO fileIO = new FileIO();
+            
+            fileIO.ReadFromFile();
+        }
+    }
+}
---
+++ T2Lab7/T2Lab7.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    class FileIO
+    {
+        public int i = 0;
+
+        public void ReadFromFile()
+        {
+            try
+            {
+                var sameName = new Dictionary<string, int>();
+
+                foreach (string item in File.ReadAllLines(@"D:\K9090\Olio\Repo\harjoitukset\nimet.txt"))
+                {
+                    i++;
+                    if (sameName.ContainsKey(item))
+                    {
+                        sameName[item] = sameName[item] + 1;
+                    }
+                    else
+                    {
+                        sameName.Add(item, 1);
+                    }
+                }
+
+                Console.WriteLine("Amount of names: {0}", i);
+
+                foreach (var pair in sameName)
+                {
+                    Console.WriteLine(pair.Key + " " + pair.Value);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found (FileNotFoundException)");
+            }
+        }
+    }
+}
