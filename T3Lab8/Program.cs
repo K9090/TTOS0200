8a7dd9c6435cc48d3ab91d64779e6c069b36ea9f
K9090 <K9090@student.jamk.fi>
2017-02-01 16:09:40 +0200

    T3Lab8 unfinished

---
+++ T3Lab8/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.T3Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                UseRegister();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void UseRegister()
+        {
+            List<Fisher> fisher = new List<Fisher>();
+            List<Fish> fish = new List<Fish>();
+
+            try
+            {
+                Fisher fisher1 = new Fisher { Name = "Erkki Tolvana", PhoneNumber = "045-6667778" };
+                Fisher fisher2 = new Fisher { Name = "Janne Tampio", PhoneNumber = "049-2634326" };
+
+                fisher.Add(fisher1);
+                fisher.Add(fisher2);
+
+                Fish fish1 = new Fish { Specie = "Salmon", Size = 160, Weight = 15 };
+                Fish fish2 = new Fish { Specie = "Pike", Size = 2000, Weight = 15 };
+
+                fish.Add(fish1);
+                fish.Add(fish2);
+
+                LocationOfCatch location1 = new LocationOfCatch { Place = "Keurusselkä", Location = "Middle-Finland" };
+                LocationOfCatch location2 = new LocationOfCatch { Place = "River Teno", Location = "Lapland" };
+
+                
+                Console.WriteLine(fisher1.ToString());
+                fisher1.AddFish(fish1);
+                fisher1.AddLocation(location1);
+
+                Console.WriteLine();
+
+                Console.WriteLine(fisher2.ToString());
+                fisher2.AddLocation(location2);
+                fisher2.AddFish(fish2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ T3Lab8/T3Lab8.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    class Fisher
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        List<Fish> Fishes { get; }
+        List<LocationOfCatch> Locations { get; }
+
+        public Fisher()
+        {
+            Fishes = new List<Fish>();
+            Locations = new List<LocationOfCatch>();
+        }
+
+        public void AddFish(Fish fish)
+        {
+            Fishes.Add(fish);
+            Console.WriteLine("\n- {0} caught {1}!", Name, fish.Specie);
+        }
+
+        public void AddLocation(LocationOfCatch location)
+        {
+            Locations.Add(location);
+            Console.WriteLine("\n- From: {0}, {1}", location.Place, location.Location);
+        }
+
+        public override string ToString()
+        {
+            return "- Name of fisher: " + Name + "\n- Phone number: " + PhoneNumber;
+        }
+    }
+
+    class Fish
+    {
+        public string Specie { get; set; }
+        public int Size { get; set; }
+        public int Weight { get; set; }
+
+        public override string ToString()
+        {
+            return "- Specie: " + Specie + "\n- Size: " + Size + " cm\n- Weight: " + Weight + " kg";
+        }
+    }
+
+    class LocationOfCatch
+    {
+        public string Place { get; set; }
+        public string Location { get; set; }
+
+        public override string ToString()
+        {
+            return "- Place of catch: " + Place + "\n- Location: " + Location;
+        }
+    }
+}
