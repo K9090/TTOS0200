a56d6d5590585b457fee3793ddaadf0b52f36b44
K9090 <K9090@student.jamk.fi>
2017-02-01 15:07:06 +0200

    T2Lab8 ok

---
+++ T2Lab8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.T2Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FillCart();
+        }
+
+        static void FillCart()
+        {
+            List<ShoppingCart> cart = new List<ShoppingCart>();
+
+            cart.Add(new Product { Name = "Coca-cola", Price = 1.59 });
+            cart.Add(new Product { Name = "Old Pascas rum", Price = 24.99 });
+            cart.Add(new Product { Name = "Cabbage", Price = 1.25 });
+
+            Console.WriteLine("Products in cart");
+            Console.WriteLine("-------------------------------------");
+            foreach (var item in cart)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ T2Lab8/T2Lab8.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    class ShoppingCart
+    {
+        List<ShoppingCart> cart = new List<ShoppingCart>();
+    }
+
+    class Product : ShoppingCart
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return "\tName of item: " + Name + " and price: " + Price;
+        }
+    }
+}
