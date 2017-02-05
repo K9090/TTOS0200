1109a6acd3a285d16a807562edf96843ccaad6be
K9090 <K9090@student.jamk.fi>
2017-02-05 18:54:19 +0200

    T4Lab8 ok enough

---
+++ T4Lab8/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.T4Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawStuff();
+        }
+
+        static void DrawStuff()
+        {
+            {
+                int i = 1;
+
+                Circle circle = new Circle();
+                circle.Name = "Circle";
+                circle.Radius = 1;
+                circle.Draw();
+                circle.PrintRadArea();
+                circle.PrintCircumference();
+                Console.WriteLine();
+
+
+                Circle circle1 = new Circle();
+                circle1.Name = "Bigger circle";
+                circle1.Radius = 3;
+                circle1.Draw();
+                circle1.PrintRadArea();
+                circle1.PrintCircumference();
+                Console.WriteLine();
+
+
+                Rectangle rectangle = new Rectangle();
+                rectangle.Name = "Rectangle";
+                rectangle.Width = 25;
+                rectangle.Height = 120;
+                rectangle.Draw();
+                rectangle.PrintRadArea();
+                rectangle.PrintCircumference();
+                Console.WriteLine();
+
+
+                Rectangle rectangle1 = new Rectangle();
+                rectangle1.Name = "Smaller rectangle";
+                rectangle1.Width = 5;
+                rectangle1.Height = 2.5;
+                rectangle1.Draw();
+                rectangle1.PrintRadArea();
+                rectangle1.PrintCircumference();
+                Console.WriteLine();
+
+                List<Shape> shapes = new List<Shape>();
+                // use polymorphis to store objects
+                shapes.Add(circle);
+                shapes.Add(circle1);
+                shapes.Add(rectangle);
+                shapes.Add(rectangle1);
+
+                for (int j = 0; j < Console.WindowWidth; j++)
+                {
+                    Console.Write("-");
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("Loop through all drawing objects in a list:");
+                foreach (Shape item in shapes)
+                {
+                    Console.WriteLine("\nShape #{0}", i);
+                    item.Draw();
+                    item.PrintRadArea();
+                    item.PrintCircumference();
+                    i++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ T4Lab8/T4Lab8.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    abstract class Shape
+    {
+        public string Name { get; set; }
+        public double area = 0;
+        public double Area
+        {
+            get { return area; }
+            set { area = value; }
+        }
+        public double Circum { get; set; }
+
+        public Shape()
+        {
+        }
+
+        public Shape(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void PrintRadArea();
+
+        public abstract void PrintCircumference();
+
+        public abstract void Draw();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+        
+        
+        private const double pi = 3.14;
+        
+
+        public override void Draw()
+        {
+            Console.WriteLine("Circle");
+        }
+
+        public override void PrintRadArea()
+        {
+            area = Radius * Radius * pi;
+            Console.WriteLine("Radius: {0}", Radius + " Area: " + Area);
+        }
+
+        public override void PrintCircumference()
+        {
+            Circum = 2 * pi * Radius;
+            Console.WriteLine("Circumference: {0}", Circum);
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public override void Draw()
+        {
+            Console.WriteLine("Rectangle");
+        }
+        public override void PrintRadArea()
+        {
+            area = Width * Height;
+            Console.WriteLine("Width: " + Width + " Height: " + Height + " Area: " + Area);
+        }
+
+        public override void PrintCircumference()
+        {
+            Circum = (Width + Height) * 2;
+            Console.WriteLine("Circumference: {0}", Circum);
+        }
+    }
+
+   /* class Shapes
+    {
+        List<Shape> shapes { get; }
+
+        public Shapes()
+        {
+            shapes = new List<Shape>();
+        }
+
+        public void AddShapes(Shape shape)
+        {
+            shapes.Add(shape);
+            Console.WriteLine("A new {0} added to list", shape.Name);
+        }
+    }*/
+}
